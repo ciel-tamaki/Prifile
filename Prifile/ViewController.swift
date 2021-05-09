266c9f1c5c2335e579401d9851a0db7fff7b5b28
tamaki annen <tmn.agenkir.luck@icloud.com>
2021-05-10 08:07:05 +0900

    Initial Commit

---
+++ Prifile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prifile
+//
+//  Created by 安念　玉希 on 2021/05/10.
+//  Copyright © 2021 安念　玉希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
